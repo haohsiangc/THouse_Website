4e08de0cd0c925e715a2ed39579c31217a91e6a0
howardjhs7 <howardjhs7@cm.nsysu.edu.tw>
2019-01-14 13:56:40 +0800

    add member detail input page

---
+++ become_member.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Join Us</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container py-5 main">
+            <div class="row">
+                <div class="col-md-12" data-aos="fade-up" data-aos-duration="500">
+                    <form>
+                        <div class="form-group row">
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" placeholder="你的名字" required>
+                            </div>
+                            <div class="col-sm-5">
+                                <input type="text" class="form-control" placeholder="手機號碼" required>
+                            </div>
+                            <div class="col-sm-5">
+                                <input type="text" class="form-control" placeholder="電子郵件" required>
+                            </div>
+                        </div>
+                    
+                        <div class="form-group row">
+                            <div class="col-sm-2">
+                                <select class="form-control" required>
+                                    <option value="基隆市">基隆市</option>
+                                    <option value="台北市">台北市</option>
+                                    <option value="新北市">新北市</option>
+                                    <option value="宜蘭縣">宜蘭縣</option>
+                                    <option value="桃園市">桃園市</option>
+                                    <option value="新竹縣">新竹縣</option>
+                                    <option value="新竹市">新竹市</option>
+                                    <option value="苗栗縣">苗栗縣</option>
+                                    <option value="台中市">台中市</option>
+                                    <option value="彰化縣">彰化縣</option>
+                                    <option value="雲林縣">雲林縣</option>
+                                    <option value="南投縣">南投縣</option>
+                                    <option value="嘉義縣">嘉義縣</option>
+                                    <option value="嘉義市">嘉義市</option>
+                                    <option value="台南市">台南市</option>
+                                    <option value="高雄市">高雄市</option>
+                                    <option value="屏東縣">屏東縣</option>
+                                    <option value="台東縣">台東縣</option>
+                                    <option value="花蓮縣">花蓮縣</option>
+                                    <option value="澎湖縣">澎湖縣</option>
+                                    <option value="連江縣">連江縣</option>
+                                    <option value="金門縣">金門縣</option>
+                                    <option value="地址" selected disabled hidden>地址</option>
+                                </select>
+                            </div>
+                            <div class="col-sm-3">
+                                <input type="text" class="form-control" placeholder="區/鄉/鎮/市" required>
+                            </div>
+                            <div class="col-sm-7">
+                                <input type="text" class="form-control" placeholder="路名/街道/巷弄/戶號/樓層">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-2">
+                                <button type="submit" class="btn btn-primary btn-lg">加入梯浩室！</button>
+                            </div>
+                            <div class="col-sm-2">
+                                <button type="reset" class="btn btn-secondary btn-lg">啊！填錯了！</button>
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    </body>
+    </html>
