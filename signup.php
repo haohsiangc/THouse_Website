446a689b0c8692f35ab745034efa689a7b675d2a
howardjhs7 <howardjhs7@cm.nsysu.edu.tw>
2019-01-17 16:55:32 +0800

    remove confirm password script

--- signup.php
+++ signup.php
@@ -131,14 +131,7 @@
             
             </form>
         </div>
-        <script src="text/javascript">
-            str1 = document.getElementById('paswd');
-            str2 = document.getElementById('confirm_paswd');
-            if(str1 == str2){
-                alert("密碼不一致，請重新確認");
-                location.href="signup.html";
-            }
-        </script>
+        
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
